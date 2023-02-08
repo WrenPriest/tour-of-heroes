9d73e2136a1663b20a27c3ea516c7fc0e78303b7
WrenPriest <wrenpriest@gmail.com>
2023-02-07 20:40:49 -0600

    Finished MessageService

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class MessagesComponent {
 
+  constructor(public messageService: MessageService) {
+  }
 }
